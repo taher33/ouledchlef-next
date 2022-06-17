5ba020d55eb865098d21e1428b81818321929240
latreche taher <latrechemedtaher@gmail.com>
2022-06-17 14:44:09 +0200

    addng context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,22 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { AppContext } from "../utils/context";
+import { FirstConnection } from "../utils/socket";
+import { useState } from "react";
+
+const socket = FirstConnection();
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
+  const [user, setUser] = useState<any>();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <AppContext.Provider value={{ user, setUser, socket: socket! }}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </AppContext.Provider>
   );
 }
 
--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,31 +1,23 @@
+import { User } from "@prisma/client";
 import { GetServerSideProps, NextPage } from "next";
 import React, { useState } from "react";
 import { Socket } from "socket.io-client";
+import { useAppContext } from "../utils/context";
 import { FirstConnection, useSubscribe } from "../utils/socket";
-
-interface User {
-  name: string;
-  email: string;
-  isConnected: boolean;
-}
-
-interface Message {
-  body: string;
-  sender: string;
-  reciver: string;
-}
-
-const socket = FirstConnection();
 
 function Chat() {
   const [messages, setMessages] = useState<string[]>([]);
   const [users, setUsers] = useState<string[]>([]);
 
+  const { socket, user } = useAppContext();
+
   function recieveMsg(msg: string) {
     console.log(msg, "msg change");
   }
 
-  const handleNewUser = () => {};
+  const handleNewUser = () => {
+    console.log("new user");
+  };
 
   useSubscribe({
     event: "chat:message",
