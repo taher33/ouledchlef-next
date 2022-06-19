1990bdc15d5fd7ddce0c1d316468213fb511b5c6
latreche taher <latrechemedtaher@gmail.com>
2022-06-19 17:37:53 +0100

    added context

---
+++ utils/context.ts
@@ -0,0 +1,25 @@
+import { createContext, Dispatch, SetStateAction, useContext } from "react";
+import { Socket } from "socket.io-client";
+
+interface State {
+  user:
+    | {
+        name: string;
+        email: string;
+        id: string;
+        imgUrl: string | undefined;
+      }
+    | undefined;
+  setUser: Dispatch<
+    SetStateAction<
+      | { name: string; email: string; id: string; imgUrl: string | undefined }
+      | undefined
+    >
+  >;
+  socket: Socket;
+}
+export const AppContext = createContext<State>({} as State);
+
+export function useAppContext() {
+  return useContext(AppContext);
+}
