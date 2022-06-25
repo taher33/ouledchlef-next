847d16ee0aba039ee61a52408866caf95382ab28
latreche taher <latrechemedtaher@gmail.com>
2022-06-25 19:38:25 +0100

    new direction

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
+const a =
+  "EAAwYqjtZAQj8BAGURCzP4BMfXFDx3jw79nOhTFzF1PUZC3S5cx8cABpKh9xUa8XrER8d32fxjG13LAOUkOKwgGTQSni9XQNtBjLTqXxCee0gcF2M8fidxeBdTm6AbcEW6TcZAMaXZBZB3qqRIusWUdQ0nFOFTgk5tvH3S0jw7SLVdwwAY9lTlbMvJLUq0ybkZD";
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" });
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,44 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import { ReactNode, useEffect, useRef, useState } from "react";
+import { motion, useDragControls } from "framer-motion";
 
 const Home: NextPage = () => {
+  const [position, setPosition] = useState({
+    top: 0,
+    left: 0,
+  });
+  const ref = useRef<HTMLDivElement>(null);
+  const dragControles = useDragControls();
+  const startDrag = (e: any) => {
+    dragControles.start(e, { snapToCursor: true });
+  };
+
   return (
     <div>
       <h1>Hello Next.js</h1>
-      <Link href="/signup">create acount</Link>
-      <br />
-      <Link href="/login">login</Link>
-      <br />
+
+      <div className="flex">
+        <div className="w-screen h-screen border relative border-black">
+          <motion.div
+            ref={ref}
+            drag
+            dragMomentum={false}
+            dragControls={dragControles}
+            className={`h-10 w-10 bg-slate-400 cursor-pointer absolute top-${position.top} left-${position.left} `}
+          >
+            <motion.div
+              drag
+              dragMomentum={false}
+              dragControls={useDragControls()}
+              dragConstraints={ref}
+              dragElastic={false}
+              className="w-4 h-4 bg-red-600"
+            ></motion.div>
+          </motion.div>
+        </div>
+        <div>controles</div>
+      </div>
     </div>
   );
 };
